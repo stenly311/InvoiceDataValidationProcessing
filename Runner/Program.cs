c9a2e851e1f8111580fdda2c84f0343c41a73fb5
martin <stenly311@gmail.com>
2024-02-19 08:48:43 +0100

    adding doc

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -56,3 +56,28 @@ processor.Process(invoices);
 
 Console.WriteLine("End");
 
+/*
+
+REQUIREMENTS LIST
+-----------------------------
+
+* VAT Normalisation
+Our ERPX is unable to process VAT numbers with non-alphanumeric characters. Please come up with a solution that removes such characters from the VAT code.
+
+* Unsupported Files
+At CustomerX our vendors sometimes send irrelevant attachments which we don’t want to deal with like logos of their company. We would like to process only PDF files.
+
+* Due Date
+Due dates should not be in the past. Please emit an error if this occurs.
+
+* Line Items Calculations
+Line items only contain unit price and quantity but not total price which is not printed on the document. Please create a new column in line items and populate it with the product of unit price and quantity.
+
+* Vendor Matching
+The ERPX requires Vendor Code to be provided with the invoice data (sample list of vendors attached).
+If the vendor is not recognised, the document should be blocked in Rossum with an error message.
+
+* Data Points for Extraction
+Should you need it, the list of fields CustomerX wishes to extract is also attached.
+ 
+ */ 
